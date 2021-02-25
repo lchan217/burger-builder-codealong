2125cca327178e4c687fe9ff3d04236d5f7284c0
Lizzie <lchan217@gmail.com>
2021-02-25 09:54:26 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       did i change
+      </div>
+    );
+  }
+}
+
+export default App;
