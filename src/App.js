9189916e03fc97bd6a2ba0d22aadf58c7647fe37
Lizzie <lchan217@gmail.com>
2021-02-25 10:35:07 -0700

    create aux and layout components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-
+import Layout from '../src/components/Layout/Layout'
+import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder'
 class App extends Component {
   render() {
     return (
       <div>
-       did i change
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
