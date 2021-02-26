e97d879383a2eaa06512dd92912d39783c9be88e
Lizzie <lchan217@gmail.com>
2021-02-26 10:03:51 -0700

    add ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,15 +3,21 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
-  // state = {
-  //   ingredients: {},
-  //   purchased: false,
-  //   price: 0
-  // }
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    purchased: false,
+    price: 0
+  }
+
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Aux>
     );
