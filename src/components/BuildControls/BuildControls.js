50c2786cb0daaf0aadf6f915796eba4d74ee6877
Lizzie <lchan217@gmail.com>
2021-03-01 09:35:00 -0700

    add more and less functionality

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -14,7 +14,13 @@ const buildControls = (props) => {
   return (
     <div className={classes.BuildControls}>
       {controls.map(control => (
-        <BuildControl key={control.type} label={control.label} />
+        <BuildControl 
+          key={control.label} 
+          label={control.label}
+          type={control.type}
+          added={() => props.ingredientAdded(control.type)}
+          removed={() => props.ingredientRemoved(control.type)}
+          disabled={props.disabled[control.type]} />
       ))}
     </div>
   );
