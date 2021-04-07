5a0b68982ebf93fdc07471fe83b4142147369b73
Lizzie <lchan217@gmail.com>
2021-04-07 16:49:17 -0600

    refactor burgerbuilder actions to sagas

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -4,6 +4,7 @@ import {
     checkAuthTimeoutSaga, 
     authUserSaga,
     authCheckStateSaga } from './auth'
+import { initIngredientsSaga } from './burgerBuilder'
 import * as actionTypes from '../actions/actionTypes'
 
 export function* watchAuth() {
@@ -11,4 +12,8 @@ export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga)
     yield takeEvery(actionTypes.AUTH_USER, authUserSaga)
     yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+}
+
+export function* watchBurgerBuilder() {
+    yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga)
 }
