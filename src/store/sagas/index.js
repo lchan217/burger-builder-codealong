688bc1038969845e26c370c951ac29a321d609e5
Lizzie <lchan217@gmail.com>
2021-04-07 17:12:58 -0600

    refactor order actions to sagas

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -5,6 +5,7 @@ import {
     authUserSaga,
     authCheckStateSaga } from './auth'
 import { initIngredientsSaga } from './burgerBuilder'
+import { purchaseBurgerSaga, fetchOrdersSaga } from './order'
 import * as actionTypes from '../actions/actionTypes'
 
 export function* watchAuth() {
@@ -16,4 +17,9 @@ export function* watchAuth() {
 
 export function* watchBurgerBuilder() {
     yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga)
+}
+
+export function* watchOrder() {
+    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga)
+    yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga)
 }
